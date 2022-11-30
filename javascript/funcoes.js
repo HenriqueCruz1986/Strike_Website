42c89ce0b6515290988acaae18410a2c5dbb205a
HenriqueCruz1986 <henriquecruz1986@gmail.com>
2022-11-29 21:23:26 -0300

    Ingremento da função menu hamburguer

--- javascript/funcoes.js
+++ javascript/funcoes.js
@@ -1,5 +1,19 @@
-function mudafoto (foto)
-		{
-			document.getElementById("icone").src = foto;
-		}	
+function mudafoto(foto) {
+	document.getElementById("icone").src = foto;
+}	
 
+function clickMenu() {
+	if (burguer.style.display == 'block') {
+		burguer.style.display = 'none'
+	} else {
+		burguer.style.display = 'block'
+	}
+}
+
+function mudouTela() {
+	if (window.innerWidth >= 768) {
+		burguer.style.display = 'block'
+	} else {
+		burguer.style.display = 'none'
+	}
+}
